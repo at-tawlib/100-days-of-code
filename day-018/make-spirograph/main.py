344171119904bcdc2b8fb8b2f3ecacfa005792b5
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2022-12-02 17:35:46 +0000

    Create make-spirograph

---
+++ day-018/make-spirograph/main.py
@@ -0,0 +1,25 @@
+# day 18 draw a spirograph using turtle
+import turtle as t
+import random
+
+tim = t.Turtle()
+t.colormode(255)
+
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    color = (r, g, b)
+    return color
+
+
+tim.speed("fastest")
+
+for _ in range(100):
+    tim.color(random_color())
+    tim.circle(100)
+    tim.setheading(tim.heading() + 10)
+
+screen = t.Screen()
+screen.exitonclick()
