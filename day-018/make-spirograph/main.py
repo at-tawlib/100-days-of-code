bf77d2f990b358784f406418c0a5bb50e92b6718
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2022-12-02 18:14:40 +0000

    Official solution

--- day-018/make-spirograph/main.py
+++ day-018/make-spirograph/main.py
@@ -7,6 +7,7 @@ t.colormode(255)
 
 
 def random_color():
+    """generates a random colour"""
     r = random.randint(0, 255)
     g = random.randint(0, 255)
     b = random.randint(0, 255)
@@ -14,12 +15,38 @@ def random_color():
     return color
 
 
+def draw_spirograph(size_of_gap):
+    """takes size of a gap,uses it to determine the number of times to draw the circle"""
+    for _ in range(int(360 / size_of_gap)):
+        tim.color(random_color())
+        tim.circle(100)
+        tim.setheading(tim.heading() + size_of_gap)
+
+
 tim.speed("fastest")
 
-for _ in range(100):
-    tim.color(random_color())
-    tim.circle(100)
-    tim.setheading(tim.heading() + 10)
+draw_spirograph(2)
+tim.penup()
+tim.setposition(200, 200)
+tim.pendown()
+
+draw_spirograph(10)
+tim.penup()
+tim.setposition(-300, -200)
+tim.pendown()
+
+draw_spirograph(20)
+tim.penup()
+tim.setposition(-300, 300)
+tim.pendown()
+
+draw_spirograph(50)
+tim.penup()
+tim.setposition(300, -300)
+tim.pendown()
+
+draw_spirograph(35)
+
 
 screen = t.Screen()
 screen.exitonclick()
