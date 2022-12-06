234f4ce7e8a8b1aa4e4cbd706dc8562ad906de6b
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2022-12-06 20:12:21 +0000

    Handle if paddle misses

--- day-022/pong-game/main.py
+++ day-022/pong-game/main.py
@@ -36,4 +36,12 @@ while game_is_on:
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
 
+    #  detect R paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+
+    # detect L paddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
+
 screen.exitonclick()
