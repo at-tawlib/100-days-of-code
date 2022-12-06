af438f649317f3d81f7fe1eff94bc97ce56590aa
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2022-12-06 12:20:57 +0000

    Detect collision with wall

--- day-020-021/snake-game/main.py
+++ day-020-021/snake-game/main.py
@@ -28,11 +28,16 @@ game_is_on = True
 while game_is_on:
     screen.update()  # update screen after action has occurred
     time.sleep(0.1)
-
     snake.move()
+
     # detect collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
         scoreboard.increase_score()
+
+    # Detect collision with wall.
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
 
 screen.exitonclick()
