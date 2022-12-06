3ae943e2a03e10a8ee01bd2bdb636d3eb1dcdb3b
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2022-12-06 12:17:57 +0000

    Add game_over function and  comments for scoreboard functions

--- day-020-021/snake-game/scoreboard.py
+++ day-020-021/snake-game/scoreboard.py
@@ -1,11 +1,12 @@
 from turtle import Turtle
-FONT = "center"
-ALIGNMENT = ("Arial", 24, "normal")
+ALIGNMENT = "center"
+FONT = ("Arial", 24, "normal")
 
 
 class Scoreboard(Turtle):
 
     def __init__(self):
+        """build a new score board with the attributes below"""
         super().__init__()
         self.score = 0
         self.color("white")
@@ -15,9 +16,16 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=FONT, font=ALIGNMENT)
+        """updates the scoreboard with the new score"""
+        self.write(f"Score: {self.score}", font=FONT, align=ALIGNMENT)
+
+    def game_over(self):
+        """prints Game Over"""
+        self.goto(0, 0)
+        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
+        """increases the score by 1, then updates it"""
         self.score += 1
         self.clear()
         self.update_scoreboard()
