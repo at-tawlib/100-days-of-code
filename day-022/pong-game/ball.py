96548fc73625973adbeebb35b49474831498d6f6
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2022-12-06 18:13:13 +0000

    Handle collision with paddle

--- day-022/pong-game/ball.py
+++ day-022/pong-game/ball.py
@@ -17,6 +17,10 @@ class Ball(Turtle):
         new_y = self.ycor() + self.y_move
         self.goto(new_x, new_y)
 
-    def bounce(self):
+    def bounce_y(self):
         """moves in the opposite direction of y"""
         self.y_move *= -1
+
+    def bounce_x(self):
+        """moves in the opposite direction of x"""
+        self.x_move *= -1
