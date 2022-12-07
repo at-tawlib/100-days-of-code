a33d5f0fb78ef2ef1f6b63372ea12f67aebf03a9
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2022-12-07 01:59:56 +0000

    Create scoreboard.py

--- day-022/pong-game/main.py
+++ day-022/pong-game/main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -13,6 +14,7 @@ screen.tracer(0)    # off animation
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 ball = Ball()
+scoreboard = Scoreboard()
 
 # listen to screen events
 screen.listen()
@@ -39,9 +41,11 @@ while game_is_on:
     #  detect R paddle misses
     if ball.xcor() > 380:
         ball.reset_position()
+        scoreboard.l_point()
 
     # detect L paddle misses
     if ball.xcor() < -380:
         ball.reset_position()
+        scoreboard.r_point()
 
 screen.exitonclick()
