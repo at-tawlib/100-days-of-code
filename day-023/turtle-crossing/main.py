71a49234ad3f1ac7daa6ff977cecf6f8daeb9140
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2022-12-07 06:36:31 +0000

    Handle game over

--- day-023/turtle-crossing/main.py
+++ day-023/turtle-crossing/main.py
@@ -10,6 +10,7 @@ screen.tracer(0)
 
 player = Player()
 car_manager = CarManager()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(player.go_up, "Up")
@@ -26,11 +27,13 @@ while game_is_on:
     for car in car_manager.all_cars:
         if player.distance(car) < 20:
             game_is_on = False
+            scoreboard.game_over()
 
     # Detect successful crossing
     if player.is_at_finish_line():
         player.go_to_start()
         car_manager.level_up()
+        scoreboard.increase_level()
 
 
 screen.exitonclick()
