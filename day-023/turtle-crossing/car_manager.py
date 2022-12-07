b9949c00776c0248856d2bf4bdebd5a704cb0f9e
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2022-12-07 05:59:27 +0000

    Handle car_manager

--- day-023/turtle-crossing/car_manager.py
+++ day-023/turtle-crossing/car_manager.py
@@ -1,7 +1,30 @@
+from turtle import Turtle
+import random
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
 class CarManager:
-    pass
+
+    def __init__(self):
+        self.all_cars = []
+
+    def create_car(self):
+        """create a new car randomly and add to all_cars list"""
+        # prevent overloading the screen with too many cars
+        random_chance = random.randint(1, 6)
+        if random_chance == 1:
+            new_car = Turtle("square")
+            new_car.shapesize(stretch_len=2, stretch_wid=1)
+            new_car.penup()
+            new_car.color(random.choice(COLORS))
+            random_y = random.randint(-250, 250)
+            new_car.goto(300, random_y)
+            self.all_cars.append(new_car)
+
+    def move_cars(self):
+        for car in self.all_cars:
+            car.backward(STARTING_MOVE_DISTANCE)
+
