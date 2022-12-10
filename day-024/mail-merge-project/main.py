b44d3830e5b0b3a883a2b8dd1f624245002ed6bf
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2022-12-10 07:01:06 +0000

    Recode after solution

--- day-024/mail-merge-project/main.py
+++ day-024/mail-merge-project/main.py
@@ -1,15 +1,13 @@
+# get names into names list
+with open("./Input/Names/invited_names.txt") as file:
+    names = file.readlines()
+
 # get contents of starting_letter
 with open("./Input/Letters/starting_letter.txt") as file:
     letter = file.read()
-
-names = []
-# get names into names list
-with open("./Input/Names/invited_names.txt") as file:
-    contents = file.readlines()
-    for line in contents:
-        names.append(line.rstrip())
-
-# create letter for each name
-for name in names:
-    with open(f"./Output/ReadyToSend/letter_for_{name}.txt", mode="w") as file:
-        file.write(letter.replace("[name]", name))
+    # create letter for each name
+    for name in names:
+        stripped_name = name.strip()
+        new_letter = letter.replace("[name]", stripped_name)
+        with open(f"./Output/ReadyToSend/letter_for_{stripped_name}.txt", mode="w") as complete_letter:
+            complete_letter.write(new_letter)
