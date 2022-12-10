8b5c331b8f7a7d6c4c75156751fa577a2254023a
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2022-12-10 13:13:53 +0000

    Extract squirrel count from file

--- day-025/README.md
+++ day-025/README.md
@@ -6,3 +6,10 @@
 - use pandas to read .csv data
 - Use padas to find the max, min, average temperature etc.
 - Use pandas to create dataframe
+
+## squirrel-data-census
+- extract data from the `2018_Central_Park_Squirrel_Census.csv`
+- get the count of the different colours of the squirrels
+- creata a dataframe for the new data
+- create a .csv file which contains the total count of each fur color
+
--- day-025/squirrel-data-census/main.py
+++ day-025/squirrel-data-census/main.py
@@ -1,3 +1,22 @@
 # Day 025
+import pandas
+COL_FUR_COLOR = "Primary Fur Color"
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census.csv")
+# get the unique fur colors
+fur_colors = data[COL_FUR_COLOR].unique()
+grey_squirrels_count = len(data[data[COL_FUR_COLOR] == "Grey"])
+red_squirrels_count = len(data[data[COL_FUR_COLOR] == "Cinnamon"])
+black_squirrels_count = len(data[data[COL_FUR_COLOR] == "Black"])
+print(grey_squirrels_count)
+print(red_squirrels_count)
+print(black_squirrels_count)
 
 
+# create dataframe
+squirrel_dict = {
+    "Fur Color": ["Grey", "Cinnamon", "Black"],
+    "Count": [grey_squirrels_count, red_squirrels_count, black_squirrels_count]
+}
+squirrel_data_frame = pandas.DataFrame(squirrel_dict)
+print(squirrel_data_frame)
+squirrel_data_frame.to_csv("squirrel_count.csv")
