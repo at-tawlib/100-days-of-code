b9c395b74b9be0a09a79f6b38bfb7ea62dc93fa7
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2022-12-13 11:37:22 +0000

    Create day-030

---
+++ day-030/README.md
@@ -0,0 +1,1 @@
+# Day 30 - Errors, Exceptions and JSON Data Improving the Password
---
+++ day-030/main.py
@@ -0,0 +1,30 @@
+# key error exception
+facebook_posts = [
+    {'Likes': 21, 'Comments': 2},
+    {'Likes': 13, 'Comments': 2, 'Shares': 1},
+    {'Likes': 33, 'Comments': 8, 'Shares': 3},
+    {'Comments': 4, 'Shares': 2},
+    {'Comments': 1, 'Shares': 1},
+    {'Likes': 19, 'Comments': 3}
+]
+total_likes = 0
+
+# if post does not have likes, pass it (coz it raises an error)
+for post in facebook_posts:
+    try:
+        total_likes = total_likes + post['Likes']
+    except KeyError:
+        pass
+print(total_likes)
+
+
+fruits = ["Apple", "Pear", "Orange"]
+# Catch the exception and make sure the code runs without crashing.
+def make_pie(index):
+    try:
+        fruit = fruits[index]
+    except IndexError:
+        print("Fruit pie")
+    else:
+        print(fruit + " pie")
+make_pie(4)
