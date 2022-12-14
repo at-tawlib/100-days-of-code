e60f308c6f423b9b275fbac2f6726e14ea92f791
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2022-12-14 14:42:45 +0000

    "Correct time conversion"

--- day-028/pomodoro/main.py
+++ day-028/pomodoro/main.py
@@ -28,9 +28,9 @@ def start_timer():
     global reps
     reps += 1
 
-    work_sec = WORK_MIN
-    short_break_sec = SHORT_BREAK_MIN
-    long_break_sec = LONG_BREAK_MIN
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
 
     if reps % 8 == 0:
         count_down(long_break_sec)
