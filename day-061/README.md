6863754fab16bd049e8afcda1d4d7623bfea5120
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-01-13 10:36:58 +0000

    Complete day-061

--- day-061/README.md
+++ day-061/README.md
@@ -1,14 +1,14 @@
 # Day 061 - Building Advanced Forms with Flask-WTForms
+- A website that holds some secrets. Only with the right username and password can you access the page with our secrets.
+- Homepage has a button to login
+- In the login page, user enters a email and password
+- if email = admin@email.com and password = 12345678, then the secret page is loaded else access denied page is loaded
+
 ## Goals
 - Installing Flask-WTF
-- Starting-Files-flask-secrets'
-- Starting-Files-flask-secrets.zip'
-- Creating Forms with Flask-WTF
-- Code Improvements for Our WTForms
+- Starting-Files-flask-secrets
 - Adding Validation to Forms with Flask-WTF
 - Receiving Form Data with WTForms
 - Inheriting Templates Using Jinja2
 - Using Flask-Bootstrap as an Inherited Template
-- Course-Fixes-Report-an-Issue
 - Flask-Bootstrap Supports WTForms
-
--- day-061/wtf-server.py
+++ day-061/wtf-server.py
@@ -1,12 +1,36 @@
+# 10-01-2023 Demo for Flask, Jinja, Flask-WTF forms, Flask Bootstrap
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from flask_bootstrap import Bootstrap
+from wtforms import EmailField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, Email
 
 app = Flask(__name__)
+Bootstrap(app)
 
+app.config['SECRET_KEY'] = "#########"
+
+class LoginForm(FlaskForm):
+    """class to create login form"""
+    email = EmailField(label='Email', validators=[DataRequired(), Email(message="Enter valid email")])
+    password = PasswordField(label='Password', validators=[DataRequired(), Length(min=8)])
+    submit = SubmitField(label='Log In')
 
 @app.route("/")
 def home():
     return render_template('index.html')
 
+@app.route('/login', methods=('GET', 'POST'))
+def login():
+    """login page"""
+    login_form = LoginForm()
+    login_form.validate_on_submit()
+    if login_form.validate_on_submit():
+        if login_form.email.data == 'admin@email.com' and login_form.password.data == '12345678':
+            return render_template('success.html')
+        else:
+            return render_template("denied.html")
+    return render_template('login.html',form=login_form)
 
 if __name__ == '__main__':
     app.run(debug=True)
